e8ebf3fc04dfb3ffa204d6a95ed73cc910544194
Aldin Kapetanovic <aldinkapetanovic@gmail.com>
2023-09-24 16:16:06 +0200

    join fixed token

--- instance.tf
+++ instance.tf
@@ -4,14 +4,13 @@ resource "aws_instance" "microk8s-node" {
   ami           = "ami-0eb260c4d5475b901"
   instance_type = "t2.medium"
 
-  # the VPC subnet
-  # subnet_id = aws_subnet.main-public-1.id
-
-  # the security group
-  # vpc_security_group_ids = [aws_security_group.allow-min-traffic.id]
-
-  # the public SSH key
+  ### the public SSH key
   key_name = aws_key_pair.microk8skey.key_name
+
+  ### root disk
+  # root_block_device {
+  #   volume_size = "20"
+  # }
 
   tags = {
     Name = "microk8s-node-${count.index + 1}"
--- output.tf
+++ output.tf
@@ -1,3 +1,12 @@
-output "microk8s-node" {
+output "microk8s-node-private_ip" {
+  value = aws_instance.microk8s-node[*].private_ip
+}
+
+
+output "microk8s-node-public_ip" {
+  value = aws_instance.microk8s-node[*].public_ip
+}
+
+output "microk8s-node-public_dns" {
   value = aws_instance.microk8s-node[*].public_dns
 }
